95d75d5a360f2fc0543f2864e079d336359b276c
mamadou <mamadou.lakhassane@gmail.com>
2020-11-13 17:04:39 +0000

    Get cases for primero v2, #7

--- src/AdaptorV2.js
+++ src/AdaptorV2.js
@@ -29,7 +29,7 @@ export function execute(...operations) {
   return state => {
     return commonExecute(
       login,
-      //...operations,
+      ...operations,
       cleanupState
     )({ ...initialState, ...state });
   };
@@ -51,7 +51,7 @@ function login(state) {
       password,
     },
   });
-  
+
   const params = {
     method: 'POST',
     url: `${url}/api/v2/tokens`,
@@ -85,6 +85,67 @@ function login(state) {
 function cleanupState(state) {
   delete state.auth;
   return state;
+}
+
+/**
+ * Get cases from Primero
+ * @public
+ * @example
+ *  getCases({
+ *    remote: true,
+ *    scope: {
+ *      transitions_created_at: 'dateRange||17-Mar-2008.17-Mar-2008',
+ *      service_response_types: 'list||referral_to_oscar',
+ *    },
+ *  }, state => {
+ *    state.lastFetch =
+ *      state.data.map(x => x.updatedAt)
+ *      .sort((a, b) => (b-a))
+ *    return state
+ *  })
+ * @function
+ * @param {object} query - an object with a query param at minimum.
+ * @param {function} callback - (Optional) Callback function
+ * @returns {Operation}
+ */
+export function getCases(query, callback) {
+  return state => {
+    const { url } = state.configuration;
+    const { token } = state.auth;
+
+    const params = {
+      method: 'GET',
+      url: `${url}/api/v2/cases`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      qs: query,
+    };
+
+    return new Promise((resolve, reject) => {
+      request(params, function (error, response, body) {
+        error = assembleError({ error, response, params });
+        if (error) {
+          reject(error);
+        } else {
+          console.log(
+            `Primero says: '${response.statusCode} ${response.statusMessage}'`
+          );
+          const resp = tryJson(body);
+          console.log(
+            `${resp.data.length} cases retrieved from request: ${JSON.stringify(
+              response.request,
+              null,
+              2
+            )}.`
+          );
+          const nextState = composeNextState(state, resp.data);
+          if (callback) resolve(callback(nextState));
+          resolve(nextState);
+        }
+      });
+    });
+  };
 }
 
 
